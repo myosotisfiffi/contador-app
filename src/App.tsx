45e91de2fe10de0850bbd0b8cb677a627396de82
SOFIA TORRES WOLTMANN <86650203068@senacrs.edu.br>
2026-05-29 11:02:02 -0300

    29/05

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+import './styles/global.css'
+import './styles/theme.css'
 
 function App() {
   const [count, setCount] = useState(0)
